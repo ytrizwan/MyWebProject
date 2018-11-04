583be480a75cf7e5ad84466775c865f0790e71b4
Rizwan <phone.asim@gmail.com>
2018-11-04 23:25:59 +1100

    Fixings 4/11/18

--- judgesLink.php
+++ judgesLink.php
@@ -5,7 +5,8 @@
 <html>
 <head>
 	<!--Head of the Index Page-->
-	<title>Pressure Cooker</title>
+	<link rel="icon" type="image/png" href="staticImages/logo_pc.png" />
+	<title>Judges</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
